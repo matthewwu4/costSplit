76ad4f37d18399e3bac52310b2d0c3c087271e4d
mattwu <mattwu@umich.edu>
2021-02-09 02:58:41 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Matthew Wu on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
